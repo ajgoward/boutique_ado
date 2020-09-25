9e6b86970e4e97a93fe6cdf66ce63efa88d30c3c
ajgoward <ajgoward@gmail.com>
2020-09-25 09:54:25 +0000

    added sixes

--- products/models.py
+++ products/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(
-        max_length=254, null=True, blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -16,22 +17,14 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-
-    class Meta:
-        verbose_name_plural = 'Catergories'
-
-    category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(
-        max_length=254, null=True, blank=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(
-        max_digits=6, decimal_places=2)
-    rating = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(
-        max_length=1024, null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
