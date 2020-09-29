7efc68361ad635ed63435550c781aedaaccf7c8b
ajgoward <ajgoward@gmail.com>
2020-09-29 21:09:55 +0000

    finished the webhooks

--- checkout/models.py
+++ checkout/models.py
@@ -21,6 +21,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, null=False, decimal_places=2, default=0)
     order_total = models.DecimalField(max_digits=10, null=False, decimal_places=2, default=0)
     grand_total = models.DecimalField(max_digits=10, null=False, decimal_places=2, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
