1e6b386c9a95485dcd3955b6a1ab8e61cb7154f5
ajgoward <ajgoward@gmail.com>
2020-09-30 19:25:43 +0000

    aws deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -170,7 +170,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ajg'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado'
     AWS_S3_REGION_NAME = 'EU'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
