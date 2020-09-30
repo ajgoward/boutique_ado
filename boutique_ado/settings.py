17eb1f7210c55e80a6d8aa3b454c797085767fcd
ajgoward <ajgoward@gmail.com>
2020-09-30 19:27:07 +0000

    aws deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -170,8 +170,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado'
-    AWS_S3_REGION_NAME = 'EU'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ajg'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
